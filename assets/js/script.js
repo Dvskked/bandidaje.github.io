9e0acafad9328582c829f8f25d70f158aa043c90
Andres Forero <163767388+Dvskked@users.noreply.github.com>
2026-01-18 16:35:31 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let docu = document.querySelector(".documentacion");
+
+function calculadoraBandidos(){
+    window.open("https://dvskked.github.io/bandido.github.io/");
+}
+
+function calculadoraBandidas(){
+    window.open("https://dvskked.github.io/bandida.github.io/");
+}
+
+function documentacion(){
+    docu.style.display = "block";
+}
